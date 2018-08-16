8f2ca8e234befc6d1dfab0f443433fb062e43fde
IsaJcode <jobisabelle.laure@gmail.com>
2018-08-17 01:44:37 +0200

    Modification to load the level from a file

--- code/constants.py
+++ code/constants.py
@@ -1,26 +1,12 @@
 """ This module provides all constants used in the code"""
 import pygame
+import os
 
 #LEVEL provides a string whih describes the labyrinth
 #0 represent free place
 #m reprensent the wall
 
-LEVEL = "000mmmmm0m00mmm \
-0m00000m0mm0m0m \
-00mm0m000m0000m \
-m000mmmm000mm0m \
-000m00000mmm00m \
-mm0mmmm0mm000mm \
-000mmm0000mmmmm \
-0m000mm0m0mm00m \
-0mm0m000m0000mm \
-mmm00mmmm0mmmmm \
-mmm0mm0m00mm00m \
-m000m00m0mm00mm \
-00mm00000000mmm \
-mmmmm0mm0mm0mmm \
-m00000mm00m0000 "
-
+LEVEL = open("level.txt", "r").read() #the game level can be easily configured in level.txt
 STARTPOINT = (0, 0)
 STOPPOINT = (448, 448)
 
